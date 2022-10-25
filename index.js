6b174ad99aafcc16accbecdb7d28ba360bf5829e
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-25 18:08:11 +0600

    Cetegory file load to server

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5500;
 
+const categoryies = require('./data/category.json');
 
 app.get('/', (req, res) => {
     res.send('Coder-e-Learnig Server Running')
+})
+
+app.get('/course-categories', (req, res) => {
+    res.send(categoryies);
 })
 
 app.listen(port, () => {
