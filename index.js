db64c396282d7d39f284afddf58d7cdecdeaaf3e
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-25 17:58:28 +0600

    Basic Server SetUp

---
+++ index.js
@@ -0,0 +1,12 @@
+const { application } = require('express');
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5500;
+
+app.get('/', (req, res) => {
+    res.send('Coder-e-Learnig Server Running')
+})
+
+app.listen(port, () => {
+    console.log("Our E-Learning Server is Running on port: ", port);
+})
