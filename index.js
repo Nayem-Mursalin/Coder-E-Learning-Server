b5cfcbe91cb95aa2f799bc9c470b698f8a8e2287
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-27 23:15:28 +0600

    Image added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('Coder-e-Learnig Server Running')
 })
 
-app.get('/course-categories', (req, res) => {
+app.get('/allcourses', (req, res) => {
     res.send(categoryies);
 })
 
